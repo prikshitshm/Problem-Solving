654a6c243a4f5be860d6af42e875ca66db6b99d5
Prikshiiiii <prikshit100x@gmail.com>
2025-01-14 22:00:38 +0530

    Create MissingNumber.cpp

---
+++ Arrays/MissingNumber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    int n= nums.size();
+    int sum=(n*(n+1))/2;
+
+    int s2=0;
+    for(int i=0;i<n;i++){
+        s2+=nums[i];
+    }
+
+    int missingNumber = sum-s2;
+    return missingNumber;
+    }
+};
